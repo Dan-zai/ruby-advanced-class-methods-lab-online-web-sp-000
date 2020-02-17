b559568862841d07d18e508f7d723f3040c3d23b
Daniel Cowpertwait <danielcowpertwait@gmail.com>
2020-02-17 10:46:51 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -30,9 +30,21 @@ end
 
 def self.find_by_name(name)
   self.all.find do |song| song.name == name
+end
+end
+  
+  
 
 def self.find_or_create_by_name(name)
-  
+  self.find_by_name(name) || self.create_by_name(name)
+end
+
+
+
+
+
+
+
 
 
 
