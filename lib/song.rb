c3c92f651605aa6220a2477d14d8ef61e1a6aa4d
Daniel Cowpertwait <danielcowpertwait@gmail.com>
2020-02-17 10:53:07 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -48,7 +48,8 @@ def self.new_from_filename(filename)
   file_array = filename.sub!(/.mp3/, '').split(" - ")
   song = self.create
   song.name = file_array[1]
-  song.artist_name = 
+  song.artist_name = file_array[0]
+  
 
 
 
